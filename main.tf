d309fbb234968eb3fc5d51cec65f8f40f27e02e7
Ondrej Sika <ondrej@ondrejsika.com>
2024-02-20 10:46:26 +0100

    feat: Setup DigitalOcean provider

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.34.1"
+    }
+  }
+}
+
+provider "digitalocean" {
+  # Configuration options
+}
