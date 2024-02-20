dfa1ee8810a2fe878d75f5744a98975980df8013
Ondrej Sika <ondrej@ondrejsika.com>
2024-02-20 13:45:01 +0100

    feat: Add cloud init example

--- main.tf
+++ main.tf
@@ -95,3 +95,37 @@ resource "digitalocean_droplet" "nginx" {
 output "nginx" {
   value = digitalocean_droplet.nginx.ipv4_address
 }
+
+resource "digitalocean_droplet" "nginx2" {
+  image  = local.IMAGE_DEBIAN_11
+  name   = "nginx2"
+  region = "fra1"
+  size   = local.SIZE_MEDIUM
+  ssh_keys = [
+    digitalocean_ssh_key.default.id,
+  ]
+  vpc_uuid  = data.digitalocean_vpc.default.id
+  user_data = <<EOF
+#cloud-config
+ssh_pwauth: yes
+password: asdfasdf2020
+chpasswd:
+  expire: false
+write_files:
+- path: /html/index.html
+  permissions: "0755"
+  owner: root:root
+  content: |
+    <h1>Hello from Cloud Init
+runcmd:
+  - |
+    apt update
+    apt install -y curl sudo git nginx
+    curl -fsSL https://ins.oxs.cz/slu-linux-amd64.sh | sudo sh
+    cp /html/index.html /var/www/html/index.html
+EOF
+}
+
+output "nginx2" {
+  value = digitalocean_droplet.nginx2.ipv4_address
+}
