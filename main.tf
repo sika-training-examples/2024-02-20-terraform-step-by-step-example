02abfc12136016742b39e241fea1521a45eb41e2
Ondrej Sika <ondrej@ondrejsika.com>
2024-02-20 11:26:14 +0100

    feat: Add data source for default VPC

--- main.tf
+++ main.tf
@@ -10,6 +10,10 @@ resource "digitalocean_ssh_key" "default" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCslNKgLyoOrGDerz9pA4a4Mc+EquVzX52AkJZz+ecFCYZ4XQjcg2BK1P9xYfWzzl33fHow6pV/C6QC3Fgjw7txUeH7iQ5FjRVIlxiltfYJH4RvvtXcjqjk8uVDhEcw7bINVKVIS856Qn9jPwnHIhJtRJe9emE7YsJRmNSOtggYk/MaV2Ayx+9mcYnA/9SBy45FPHjMlxntoOkKqBThWE7Tjym44UNf44G8fd+kmNYzGw9T5IKpH1E1wMR+32QJBobX6d7k39jJe8lgHdsUYMbeJOFPKgbWlnx9VbkZh+seMSjhroTgniHjUl8wBFgw0YnhJ/90MgJJL4BToxu9PVnH ondrejsika"
 }
 
+data "digitalocean_vpc" "default" {
+  region = "fra1"
+}
+
 resource "digitalocean_droplet" "example" {
   image  = local.IMAGE_DEBIAN_11
   name   = "example"
@@ -18,6 +22,7 @@ resource "digitalocean_droplet" "example" {
   ssh_keys = [
     digitalocean_ssh_key.default.id,
   ]
+  vpc_uuid = data.digitalocean_vpc.default.id
 }
 
 output "ip" {
